6fbc31f0080ed5a87582e77a5c968847ef3b9a8e
Worlanyo <wadom26@gmail.com>
2025-07-02 14:21:26 -0500

    Initital commit

---
+++ src/test/java/com/dashboard/app/personal_dashboard/PersonalDashboardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dashboard.app.personal_dashboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PersonalDashboardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
